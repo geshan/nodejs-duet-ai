43fa9af934c053f12297f6904607d44e74da119a
Geshan Manandhar <geshan@simplywallst.com>
2023-12-17 22:30:52 +1100

    better docker file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 8088 || process.env.PORT;
+const port = 8080 || process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send({ message: 'ok' });
